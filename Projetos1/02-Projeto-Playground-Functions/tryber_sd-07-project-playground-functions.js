2790a03e63ea3f4022382d6bd80cdceeb05a8ec6
michael-petterson-06 <michaelpetterson06l@gmailo.com>
2020-09-17 21:40:53 -0300

    renomeando index

--- Projetos1/02-Projeto-Playground-Functions/tryber_sd-07-project-playground-functions.js
+++
@@ -1,9 +0,0 @@
-let numero1 = 1;
-let numero2 = 10;
-let letra1 = "a";
-let letra2 = "a"; 
-
-let verificar1 = numero1 == numero2;
-let verificar2 = letra1 == letra2;
-compara = verificar1 && verificar2;
-console(compara);
