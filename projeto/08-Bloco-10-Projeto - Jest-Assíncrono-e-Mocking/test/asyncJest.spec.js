a5c06bcb75f3cbf5db364d60f2b77df24ab84af4
michael-petterson-06 <michaelpetterson06l@gmailo.com>
2021-03-20 17:19:25 -0300

    primeiro e segundo requisito

--- projeto/08-Bloco-10-Projeto - Jest-Assíncrono-e-Mocking/test/asyncJest.spec.js
+++ projeto/08-Bloco-10-Projeto - Jest-Assíncrono-e-Mocking/test/asyncJest.spec.js
@@ -10,13 +10,22 @@ a função recebe como parâmetro true e false, respectivamente.
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
+
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
+  test("atende", async () => {
     // Insira seu teste assíncrono aqui
+    const resolve = await answerPhone(true);
+    expect(resolve).toBe('Oi!');
   });
-  test("ocupado", () => {
-    assert.fail();
+
+  test("ocupado", async () => {
     // Insira seu teste assíncrono aqui
+    try {
+      await answerPhone(false);
+    }
+    catch (reject) {
+      expect(reject).toBe('Infelizmente não podemos atender...')
+    }
   });
 });
+
